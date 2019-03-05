b7bba5d7857205b2437160b76233b3dae232d20c
Bernd Mayr <bma@wfp.com>
2019-03-05 09:56:50 +0100

    Erste Version BerndControl

--- ProjektEins/ProjektEins/Form1.Designer.cs
+++ ProjektEins/ProjektEins/Form1.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.andreControl1 = new ProjektEins.AndreControl();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.andreControl1 = new ProjektEins.AndreControl();
+            this.berndControl1 = new ProjektEins.BerndControl();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -46,70 +48,88 @@
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.Size = new System.Drawing.Size(600, 366);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.andreControl1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(792, 421);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(592, 340);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "André";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // andreControl1
+            // 
+            this.andreControl1.BackColor = System.Drawing.SystemColors.Window;
+            this.andreControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.andreControl1.Location = new System.Drawing.Point(2, 2);
+            this.andreControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.andreControl1.Name = "andreControl1";
+            this.andreControl1.Size = new System.Drawing.Size(588, 336);
+            this.andreControl1.TabIndex = 0;
+            // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 421);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(592, 340);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Victor";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(792, 421);
+            this.tabPage3.Size = new System.Drawing.Size(592, 340);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Viktor";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Controls.Add(this.berndControl1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(792, 421);
+            this.tabPage4.Size = new System.Drawing.Size(592, 340);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Bernd";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // andreControl1
+            // berndControl1
             // 
-            this.andreControl1.BackColor = System.Drawing.SystemColors.Window;
-            this.andreControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.andreControl1.Location = new System.Drawing.Point(3, 3);
-            this.andreControl1.Name = "andreControl1";
-            this.andreControl1.Size = new System.Drawing.Size(786, 415);
-            this.andreControl1.TabIndex = 0;
+            this.berndControl1.BackColor = System.Drawing.SystemColors.Window;
+            this.berndControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.berndControl1.Location = new System.Drawing.Point(0, 0);
+            this.berndControl1.Name = "berndControl1";
+            this.berndControl1.Size = new System.Drawing.Size(592, 340);
+            this.berndControl1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -122,6 +142,7 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
         private AndreControl andreControl1;
+        private BerndControl berndControl1;
     }
 }
 
